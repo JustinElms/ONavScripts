3dc57519a11ae9a20b66390838419b2739a30f8c
JustinElms <justin.elms@dfo-mpo.gc.ca>
2021-11-24 13:22:04 +0000

    Reorganized directory structure

--- profiling_scripts/api_profile_testing.py
+++ profiling_scripts/api_profile_testing.py
@@ -8,74 +8,91 @@ from urllib.error import HTTPError
 from urllib.parse import urlencode
 from urllib.request import urlopen
 
-class ONav_API_Proiler():
+class ONav_API_Profiler():
 
-    def __init__(self, base_url, config_url, enable_logging = True, max_attempts = 3):
+
+    def __init__(self, base_url, config_url, enable_logging = True, max_attempts = 3, max_time = 120):
         self.base_url = base_url     
         self.logging = logging
         self.max_attempts = max_attempts
+        self.max_time = max_time
 
         if enable_logging:
-            logging.basicConfig(filename='api_profile_testing.log', level=logging.DEBUG)
+            logging.basicConfig(
+                filename='api_profile_testing.log', 
+                level=logging.DEBUG, 
+                format='%(asctime)s %(levelname)s \n %(message)s', 
+                datefmt='%H:%M:%S'
+            )
+            logging.info('****************** Starting Profiler Tests ******************')
 
         self.datasets = self.get_datasets()
         with open(config_url) as f:
             self.test_config = json.load(f)   
+
 
     def send_req(self, url):
         for i in range(self.max_attempts):
             logging.info('Attempt ' + str(i+1) + ':')
             start_time = time.time()
             try:
-                resp = requests.get(url, timeout=120)
+                resp = requests.get(url, timeout=self.max_time)
+                if resp.status_code == 200:
+                    end_time = time.time()
+                    logging.info('*** Response recieved. ***\n Total request time: ' + str(end_time - start_time))
+                    return resp
+                else:
+                    logging.warning('*** Request failed. ***')  
             except requests.ReadTimeout:
                 logging.warning('*** Request timed out. ***')
-                
-            if resp.status_code == 200:
-                end_time = time.time()
-                logging.info('*** Response recieved. ***')
-                logging.info('Total time: ' + str(end_time - start_time))
-                return resp
-            else:
-                logging.warning('*** Request failed. ***')  
+            except requests.exceptions.ConnectionError:
+                logging.warning('*** Connection aborted. ***')
+            
         logging.critical('Could not complete request after ' + str(self.max_attempts) + ' attempt(s).')
 
+
     def get_datasets(self):
-        logging.info('Requesting dataset info...')
+        logging.info('Requesting dataset meta data...')
         data = self.send_req(self.base_url + 'datasets/')
         return [d for d in json.loads(data.content)]
+
 
     def get_variables(self, dataset):
         logging.info('Requesting variables...')
         data = self.send_req(self.base_url + f'variables/?dataset={dataset}')
         return [d for d in json.loads(data.content)]
 
+
     def get_timestamps(self, dataset, variable): 
         logging.info('Requesting timestamps...')
         data = self.send_req(self.base_url + f"timestamps/?dataset={dataset}&variable={variable}")  
         return [d for d in json.loads(data.content)]       
+
 
     def get_depths(self, dataset, variable):
         logging.info('Requesting depths...')
         data = self.send_req(self.base_url +f"depth/?dataset={dataset}&variable={variable}")
         return [d for d in json.loads(data.content)]
 
+
     def get_quantum(self,ds):
         ds_data = next(item for item in self.datasets if item["id"] == ds)
         return ds_data['quantum']
 
+
     def get_plot(self,query):
         logging.info('Requesting plot...')
         self.send_req(self.base_url + 'plot/?' + urlencode({'query': json.dumps(query)}) + '&format=json')
+
 
     def profile_test(self):
         logging.info('****************** Profile Plot Tests ******************')
         config = self.test_config['profile_plot']
 
         for ds in config['datasets']:
-            logging.info("Dataset: " + ds)
+            logging.info("\nDataset: " + ds + "\n")
             quantum = self.get_quantum(ds)
-            for v in config['variables']:
+            for v in config['datasets'][ds]['variables']:
                 logging.info("Variable: " + v)
                 timestamps = self.get_timestamps(ds,v)
                 self.get_plot({
@@ -90,16 +107,19 @@ class ONav_API_Proiler():
                     "variable" : v
                 })
     
+
     def virtual_mooring_test(self):
         logging.info('****************** Virtual Mooring Plot Tests ******************')
         config = self.test_config['vm_plot']
 
         for ds in config['datasets']:
-            logging.info("Dataset: " + ds)
+            logging.info("\nDataset: " + ds + "\n")
             quantum = self.get_quantum(ds)
-            for v in config['variables']:
+            for v in config['datasets'][ds]['variables']:
                 logging.info("Variable: " + v)
                 timestamps = self.get_timestamps(ds,v)
+                start_idx = len(timestamps) - 10 # len(timestamps) - config['datasets'][ds]['time_range']
+
                 self.get_plot({
                     "colormap" : "default",
                     "dataset" : ds,
@@ -110,20 +130,21 @@ class ONav_API_Proiler():
                     "quantum" : quantum,
                     "scale" : "-5,30,auto",
                     "showmap" : 0,
-                    "starttime" : timestamps[0]['id'], 
+                    "starttime" : timestamps[start_idx]['id'], 
                     "station" : config['station'], 
                     "type" : "timeseries",
                     "variable" : v
                 })
+
 
     def transect_test(self):
         logging.info('****************** Transect Plot Tests ******************')
         config = self.test_config['transect_plot']
 
         for ds in config['datasets']:
-            logging.info("Dataset: " + ds)
+            logging.info("----------\nDataset: " + ds + "\n----------")
             quantum = self.get_quantum(ds)
-            for v in config['variables']:
+            for v in config['datasets'][ds]['variables']:
                 logging.info("Variable: " + v)
                 timestamps = self.get_timestamps(ds,v)
                 self.get_plot({
@@ -144,16 +165,19 @@ class ONav_API_Proiler():
                     "variable" : v
                 })
 
+
     def hovmoller_test(self):
         logging.info('****************** Hovmoller Plot Tests ******************')
         config = self.test_config['hovmoller_plot']
 
         for ds in config['datasets']:
-            logging.info("Dataset: " + ds)
+            logging.info("\nDataset: " + ds + "\n")
             quantum = self.get_quantum(ds)
-            for v in config['variables']:
+            for v in config['datasets'][ds]['variables']:
                 logging.info("Variable: " + v)
                 timestamps = self.get_timestamps(ds,v)
+                start_idx = len(timestamps) - 10 # len(timestamps) - config['datasets'][ds]['time_range']
+
                 self.get_plot({
                     "colormap" : "default",
                     "dataset" : ds,
@@ -165,19 +189,20 @@ class ONav_API_Proiler():
                     "quantum" : quantum,
                     "scale" : "-5,30,auto",
                     "showmap" : 1,
-                    "starttime" : timestamps[0]['id'],
+                    "starttime" : timestamps[start_idx]['id'],
                     "type" : "hovmoller",
                     "variable" : v
                 })
+
 
     def area_test(self):
         logging.info('****************** Starting Area Plot Tests ******************')
         config = self.test_config['area_plot']
 
         for ds in config['datasets']:
-            logging.info("Dataset: " + ds)
+            logging.info("\nDataset: " + ds + "\n")
             quantum = self.get_quantum(ds)
-            for v in config['variables']:
+            for v in config['datasets'][ds]['variables']:
                 logging.info("Variable: " + v)
                 timestamps = self.get_timestamps(ds,v)
                 self.get_plot({
@@ -211,9 +236,10 @@ class ONav_API_Proiler():
                     "variable" : v
                 })                
 
+
     def run(self):
         start_time = time.time()
-        logging.info('Profile testing start time: ' + str(start_time))
+        logging.info('Profile testing start time: ' + time.ctime(start_time) + ' (' + str(start_time) + ')')
 
         self.profile_test()
         self.virtual_mooring_test()
@@ -222,14 +248,13 @@ class ONav_API_Proiler():
         self.area_test()
 
         end_time = time.time()
-        logging.info('Profile testing end time: ' + str(end_time))
-        total_time = end_time - start_time
-        logging.info('Time to complete all tests: ' + str(total_time))
+        logging.info('Profile testing start time: ' + time.ctime(end_time) + ' (' + str(end_time) + ')')
+        logging.info('Time to complete all tests: ' + str(end_time - start_time))
 
 if __name__ == '__main__':
     
-    #api_profiler = ONav_API_Proiler('http://lxc-on-03.ent.dfo-mpo.ca:5000/api/v1.0/', 'api_testing_config.json')
-    api_profiler = ONav_API_Proiler('https://navigator.oceansdata.ca/api/v1.0/', 'api_testing_config.json')
-    #api_profiler = ONav_API_Proiler('http://durmstrang.ent.dfo-mpo.ca:5000/api/v1.0/', 'api_testing_config.json', enable_logging=False)
+    #api_profiler = ONav_API_Profiler('http://lxc-on-02.ent.dfo-mpo.ca:5000/api/v1.0/', '/home/ubuntu/ONavScripts/profiling_scripts/api_testing_config.json', max_time = 200)
+    api_profiler = ONav_API_Profiler('http://durmstrang.ent.dfo-mpo.ca:5000/api/v1.0/', '/home/ubuntu/ONavScripts/profiling_scripts/api_testing_config.json', max_time = 200)
+    #api_profiler = ONav_API_Profiler('http://navigator.oceansdata.ca/api/v1.0/', '/home/ubuntu/ONavScripts/profiling_scripts/api_testing_config.json')
     api_profiler.run()
     
